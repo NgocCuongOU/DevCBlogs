4ec94fdfaf4db9c397933d549b428af7f617d500
NgocCuongFE <ngoccuong.cao@inotek.us>
2022-04-14 17:11:48 +0700

    create blogpost  component

---
+++ pull_request_template.md
@@ -0,0 +1,33 @@
+# Description
+
+Please include a summary of the change and which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change.
+
+## What I've done
+
+Write what you did within this PR.
+
+## UI before / after
+
+If the UI or behavior changes, share before / after screenshots or videos.
+
+## Testing
+
+Write test items and test methods.
+
+## Review perspective
+
+This is just a guideline.
+
+- Does it work as expected?
+- Is there a better way to write JS/TS/CSS/React?
+- Is there a better way to design it?
+- Are there any differences in writing style, naming, directory structure, etc. from other parts?
+- Is the granularity of functions and components appropriate?
+- etc...
+
+# Checklist:
+
+- [ ] My code follows the style guidelines of this project
+- [ ] I have performed a self-review of my own code
+- [ ] I have commented my code, particularly in hard-to-understand areas
+- [ ] My changes generate no new warnings
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,38 @@
 <template>
-  <div><h1>Hello</h1></div>
+  <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import BlogPost from '../components/BlogPost/BlogPost.vue'
 
 export default defineComponent({
+  name: 'Home',
+  components: { BlogPost },
   data() {
-    return {}
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        blogPost:
+          'Weekly blog articles with all things programming including HTML, CSS, Javascript and more. Register today to never miss a post!',
+        welcomeScreen: true,
+        photo: 'coding'
+      },
+      sampleBlogPost: [
+        {
+          title: 'This is filter title!',
+          blogHTML: 'This is a filter blog post title',
+          blogCoverPhoto: 'beautiful-stories'
+        },
+        {
+          title: 'This is filter title2!',
+          blogHTML: 'This is a filter blog post title 2',
+          blogCoverPhoto: 'designed-for-everyone'
+        }
+      ]
+    }
   },
   mounted() {}
 })
